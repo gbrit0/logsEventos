082305f03f396af5b6ee0d20764cb276da5cb6fb
Gabriel <gbrito341@gmail.com>
2024-08-06 15:05:56 -0300

    Mover os .bash para a pasta de scripts

---
+++ script/screen_eventlogs.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+screen -XS eventlogs quit
+screen -dmS eventlogs 
+screen -S eventlogs -p 0 -X stuff 'bash /eventlogs/scripts/servidor_eventlogs.bash'
---
+++ script/servidor_eventlogs.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /eventlogs
+while :
+do
+	kill pgrep -f "python3 /eventlogs/processarSolicitacoesDeLogs.py"
+	echo "Iniciando eventlogs"
+	python3 /eventlogs/processarSolicitacoesDeLogs.py >> /eventlogs/log_do_eventlogs.log
+	sleep 5
+done
