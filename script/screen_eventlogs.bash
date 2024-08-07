90cc4b58642ca80e3247e115c87699ef00d06f04
Gabriel <gbrito341@gmail.com>
2024-08-07 11:00:16 -0300

    Teste controle de versão via extensão github no vscode

--- script/screen_eventlogs.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-screen -XS eventlogs quit
-screen -dmS eventlogs 
-screen -S eventlogs -p 0 -X stuff 'bash /eventlogs/scripts/servidor_eventlogs.bash'
--- servidor_eventlogs.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /eventlogs
-while :
-do
-	kill pgrep -f "python3 /eventlogs/processarSolicitacoesDeLogs.py"
-	echo "Iniciando eventlogs"
-	python3 /eventlogs/processarSolicitacoesDeLogs.py >> /eventlogs/log_do_eventlogs.log
-	sleep 5
-done
