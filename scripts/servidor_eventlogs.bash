f41befeb6f41fee93c14db3ed1b10c4546305df0
Gabriel <gbrito341@gmail.com>
2024-08-13 17:43:27 -0300

    Corrigindo a adição de datas no log

--- scripts/servidor_eventlogs.bash
+++ scripts/servidor_eventlogs.bash
@@ -3,9 +3,9 @@ cd /eventlogs
 while :
 do
 	# kill pgrep -f "python3 /eventlogs/processarSolicitacoesDeLogs.py"
-	printf "$date Iniciando eventlogs"
+	printf "$(date) Iniciando eventlogs"
 	source /eventlogs/venv/bin/activate
 	python3 /eventlogs/processarSolicitacoesDeLogs.py >> /eventlogs/log_do_eventlogs.log
-	printf "$date Fim da execução\n"
+	printf "$(date) Fim da execução\n"
 	sleep 1800
 done
