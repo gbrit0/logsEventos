910248bc3ea6b0f08326c7cd9b7f5bd9d424b3d6
Gabriel <gbrito341@gmail.com>
2024-08-13 17:39:04 -0300

    Adicionei a datagem no início e fim da execução de uma chamada para calcular melhor o tempo de execução

--- scripts/servidor_eventlogs.bash
+++ scripts/servidor_eventlogs.bash
@@ -3,8 +3,9 @@ cd /eventlogs
 while :
 do
 	# kill pgrep -f "python3 /eventlogs/processarSolicitacoesDeLogs.py"
-	echo "Iniciando eventlogs"
+	printf "$date Iniciando eventlogs"
 	source /eventlogs/venv/bin/activate
 	python3 /eventlogs/processarSolicitacoesDeLogs.py >> /eventlogs/log_do_eventlogs.log
+	printf "$date Fim da execução\n"
 	sleep 1800
 done
