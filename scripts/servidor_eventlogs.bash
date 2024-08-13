e02b14df718cffd55a87b09ab74e0a0c7f7f1de0
Gabriel <gbrito341@gmail.com>
2024-08-13 18:19:01 -0300

    Ajustes na impressão de data para o servidor_eventlogs.bash

--- scripts/servidor_eventlogs.bash
+++ scripts/servidor_eventlogs.bash
@@ -3,7 +3,7 @@ cd /eventlogs
 while :
 do
 	# kill pgrep -f "python3 /eventlogs/processarSolicitacoesDeLogs.py"
-	printf "$(date) Iniciando eventlogs"
+	printf "\n$(date) Iniciando eventlogs\n"
 	source /eventlogs/venv/bin/activate
 	python3 /eventlogs/processarSolicitacoesDeLogs.py >> /eventlogs/log_do_eventlogs.log
 	printf "$(date) Fim da execução\n"
