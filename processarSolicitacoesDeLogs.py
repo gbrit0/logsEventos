e4a2d102c6ebee2e7c06a4be94ef686cad23e1c3
Gabriel <gbrito341@gmail.com>
2024-08-13 13:13:54 -0300

    versão rodando

--- processarSolicitacoesDeLogs.py
+++ processarSolicitacoesDeLogs.py
@@ -237,7 +237,7 @@ def main():
          print(e)
    except mysql.connector.errors.OperationalError as e:
       pass
-   else:
+   finally:
       with mysql.connector.connect(user=os.environ['MYSQL_USER'],
                      password=os.environ['MYSQL_PASSWORD'],
                      host=os.environ['MYSQL_HOST'],
